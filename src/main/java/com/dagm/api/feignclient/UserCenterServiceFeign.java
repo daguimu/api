d2a19c498e4dbb8f4813252018e20200baa4199c
daguimu <1712066621@qq.com>
2020-02-16 17:57:25 +0800

    增加 userCenter api

---
+++ src/main/java/com/dagm/api/feignclient/UserCenterServiceFeign.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 maoyan.com
+ * All rights reserved.
+ *
+ */
+package com.dagm.api.feignclient;
+
+import com.dagm.api.dto.UserDTO;
+import com.dagm.devtool.res.BaseResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 短信服务feign
+ *
+ * @author Guimu
+ * @date 2020/01/08
+ */
+@FeignClient(value = "user-center")
+public interface UserCenterServiceFeign {
+
+    /**
+     * 根据token获取userDto
+     *
+     * @param token 用户token
+     * @return UserDTO 用户信息
+     * @author Guimu
+     * @date 2020/2/16
+     */
+    @GetMapping(value = "/inner/getUserByToken")
+    BaseResult<UserDTO> getUserByToken(@RequestParam(value = "token") String token);
+
+    /**
+     * 根据userId获取userDto
+     *
+     * @param userId 用户id
+     * @return UserDTO 用户信息
+     * @author Guimu
+     * @date 2020/2/16
+     */
+    @GetMapping(value = "/inner/getUserById")
+    BaseResult<UserDTO> getUserById(@RequestParam(value = "userId") Long userId);
+
+
+}
